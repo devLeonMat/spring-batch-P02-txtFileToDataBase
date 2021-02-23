04f24b1d1371b593e3e9c4596122daeaceaa7721
leon matias roslin <leonmatias1991@gmail.com>
2021-02-23 11:41:30 -0500

    upload basic project

---
+++ src/main/java/com/rleon/springbatchP01txtFileToDataBase/SpringBatchP01TxtFileToDataBaseApplication.java
@@ -0,0 +1,13 @@
+package com.rleon.springbatchP01txtFileToDataBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchP01TxtFileToDataBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchP01TxtFileToDataBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rleon/springbatchP01txtFileToDataBase/SpringBatchP01TxtFileToDataBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rleon.springbatchP01txtFileToDataBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchP01TxtFileToDataBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
